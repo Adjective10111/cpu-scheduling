ec3639cea293305c952530680e8d7a74e734b567
Adjective10111 <mm.80xefatzadeh@gmail.com>
2022-01-02 13:52:46 +0330

    Added calculating functions to SchedulingAlgorithm

--- src/main/java/uni/os/cpuscheduling/SchedulingAlgorithm.java
+++ src/main/java/uni/os/cpuscheduling/SchedulingAlgorithm.java
@@ -1,9 +1,14 @@
 package uni.os.cpuscheduling;
 
-import java.util.List;
-
 public interface SchedulingAlgorithm {
 	void getNewProcess(Process process);
-	void process();
 	private void selectProcess() { }
+	void process();
+	
+	int calcTotalTime();
+	double calcThroughput();
+	int calcAverageWaitingTime();
+	int calcAverageTurnaroundTime();;
+	int calcAverageResponseTime();
+	// todo CPU Utilization
 }
