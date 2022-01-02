6197bea116aac96a9e9f0f7b39f26599a5cad856
Adjective10111 <mm.80xefatzadeh@gmail.com>
2022-01-02 15:30:17 +0330

    tweaked RequestGenerator

---
+++ src/main/java/uni/os/cpuscheduling/model/PreemptiveSJF.java
@@ -0,0 +1,38 @@
+package uni.os.cpuscheduling.model;
+
+public class PreemptiveSJF implements SchedulingAlgorithm {
+	@Override
+	public void getNewProcess(Process process) {
+	
+	}
+	
+	@Override
+	public void process() {
+	
+	}
+	
+	@Override
+	public int calcTotalTime() {
+		return 0;
+	}
+	
+	@Override
+	public double calcThroughput() {
+		return 0;
+	}
+	
+	@Override
+	public int calcAverageWaitingTime() {
+		return 0;
+	}
+	
+	@Override
+	public int calcAverageTurnaroundTime() {
+		return 0;
+	}
+	
+	@Override
+	public int calcAverageResponseTime() {
+		return 0;
+	}
+}
--- src/main/java/uni/os/cpuscheduling/model/RequestGenerator.java
+++ src/main/java/uni/os/cpuscheduling/model/RequestGenerator.java
@@ -1,77 +1,86 @@
 package uni.os.cpuscheduling.model;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RequestGenerator {
-	public static void generateProcessData(){
-		try{
-			Formatter writeCSV = new Formatter("processes_data.csv");
-			for(int i=0; i<=10; i++){
-				int randomPriority = ThreadLocalRandom.current().nextInt(0, 255 + 1);
-				int randomArrival = ThreadLocalRandom.current().nextInt(0, 3000000 + 1);
-				int randomBurst = ThreadLocalRandom.current().nextInt(1, 30 + 1);
-				if(i==0){
-					writeCSV.format("%s,%s,%s,%s\n","ID", "ArrivalTime", "Priority", "BurstTime");
-				}else{
-					writeCSV.format("%d,%d,%d,%d\n", i, randomArrival, randomPriority, randomBurst );
-				}
-			}
+	public static int number_of_processes = 100;
+	public static int max_burst = 30;
+	
+	public static void generateProcessData() {
+		try {
+			var writeCSV = new Formatter("processes_data.csv");
+			generateProcessData(writeCSV);
 			writeCSV.close();
-		} catch (Exception e){
-			System.out.println("An error has occurred for generating the processes");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-
+	
+	private static void generateProcessData(Formatter writer) {
+		writer.format("%s,%s,%s,%s\n", "ID", "ArrivalTime", "Priority", "BurstTime");
+		for (int process_id = 1; process_id <= number_of_processes; process_id++) {
+			int randomPriority = ThreadLocalRandom.current().nextInt(0, 255 + 1);
+			int randomArrival = ThreadLocalRandom.current().nextInt(0, max_burst * number_of_processes + 1);
+			int randomBurst = ThreadLocalRandom.current().nextInt(1, max_burst + 1);
+			writer.format("%d,%d,%d,%d\n", process_id, randomArrival, randomPriority, randomBurst);
+		}
+	}
+	
 	public static Queue<Process> readProcessData() {
-		ArrayList<Process> processes = new ArrayList<>();
 		try {
-			File fileProcesses = new File("processes_data.csv");
-			Scanner readingCSV = new Scanner(fileProcesses);
-			int counter = 1;
-			while (readingCSV.hasNextLine()){
-				if(counter != 1){
-					String line = readingCSV.nextLine();
-					String[] stringProcess = line.split(",");
-					Process process = new Process(Integer.parseInt(stringProcess[0]), Integer.parseInt(stringProcess[1]), Integer.parseInt(stringProcess[2]), Integer.parseInt(stringProcess[3]));
-					processes.add(process);
-				}else{
-					readingCSV.nextLine();
-				}
-				counter++;
-			}
+			var fileProcesses = new File("processes_data.csv");
+			if (!fileProcesses.exists())
+				generateProcessData();
+			
+			var readingCSV = new Scanner(fileProcesses);
+			var processes = readProcessData(readingCSV);
 			readingCSV.close();
-		}catch (Exception e){
+			
+			processes.sort(Process::compareTo);
+			return new PriorityQueue<>(processes);
+		} catch (Exception e) {
 			e.printStackTrace();
+			return null;
 		}
-		processes.sort(Process::compareTo);
-		return new PriorityQueue<Process>(processes);
 	}
-
-
+	
+	
 	public static Queue<Process> readProcessData(String filename) {
-		ArrayList<Process> processes = new ArrayList<>();
 		try {
-			File fileProcesses = new File(filename);
-			Scanner readingCSV = new Scanner(fileProcesses);
-			int counter = 1;
-			while (readingCSV.hasNextLine()){
-				if(counter != 1){
-					String line = readingCSV.nextLine();
-					String[] stringProcess = line.split(",");
-					Process process = new Process(Integer.parseInt(stringProcess[0]), Integer.parseInt(stringProcess[1]), Integer.parseInt(stringProcess[2]), Integer.parseInt(stringProcess[3]));
-					processes.add(process);
-				}else{
-					readingCSV.nextLine();
-				}
-				counter++;
-			}
+			var readingCSV = new Scanner(new File(filename));
+			var processes = readProcessData(readingCSV);
 			readingCSV.close();
-		}catch (Exception e){
+			
+			processes.sort(Process::compareTo);
+			return new PriorityQueue<>(processes);
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+			return null;
 		}
-		processes.sort(Process::compareTo);
-		return new PriorityQueue<Process>(processes);
 	}
+	
+	private static ArrayList<Process> readProcessData(Scanner reader) {
+		// skip the info line
+		reader.nextLine();
+		
+		var processes = new ArrayList<Process>();
+		while (reader.hasNextLine()) {
+			var line = reader.nextLine();
+			var stringProcess = line.split(",");
+			Process process = new Process(
+					Integer.parseInt(stringProcess[0]),
+					Integer.parseInt(stringProcess[1]),
+					Integer.parseInt(stringProcess[2]),
+					Integer.parseInt(stringProcess[3])
+			);
+			processes.add(process);
+		}
+		
+		processes.sort(Process::compareTo);
+		return processes;
+	}
+	
 }
