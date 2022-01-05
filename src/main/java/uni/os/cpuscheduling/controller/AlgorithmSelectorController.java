c04ce7d792832344657d1ac273c4d36d82519615
Adjective10111 <mm.80xefatzadeh@gmail.com>
2022-01-05 18:14:22 +0330

    tweaked a bit

--- src/main/java/uni/os/cpuscheduling/controller/AlgorithmSelectorController.java
+++ src/main/java/uni/os/cpuscheduling/controller/AlgorithmSelectorController.java
@@ -2,15 +2,22 @@ package uni.os.cpuscheduling.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 
 public class AlgorithmSelectorController implements Controller {
 	@FXML
 	private ImageView back;
+	@FXML
+	private Button select;
 	
 	@FXML
 	void initialize() {
 		back.setOnMouseClicked(mouseEvent -> changeScene("main-menu"));
+		select.setOnAction(actionEvent -> {
+			// todo: select
+			changeScene("main-menu");
+		});
 	}
 	
 	@Override
