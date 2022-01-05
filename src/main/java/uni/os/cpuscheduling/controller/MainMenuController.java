a5fea9311826b4308fb663983ed49f9155617ab8
Adjective10111 <mm.80xefatzadeh@gmail.com>
2022-01-05 18:03:33 +0330

    changed some names

--- src/main/java/uni/os/cpuscheduling/controller/MainMenuController.java
+++ src/main/java/uni/os/cpuscheduling/controller/MainMenuController.java
@@ -3,8 +3,9 @@ package uni.os.cpuscheduling.controller;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import uni.os.cpuscheduling.model.OperatingSystem;
+import uni.os.cpuscheduling.model.RequestGenerator;
 
 public class MainMenuController implements Controller {
 	@FXML
@@ -12,7 +13,7 @@ public class MainMenuController implements Controller {
 	@FXML
 	private Button generate_processes;
 	@FXML
-	private ImageView configure_algorithms;
+	private ImageView select_algorithms;
 	@FXML
 	private ImageView configure_process_generator;
 	@FXML
@@ -21,6 +22,18 @@ public class MainMenuController implements Controller {
 	@FXML
 	protected void onHelloButtonClick() {
 		start_simulation.setText("Welcome to JavaFX Application!");
+	}
+	
+	@FXML
+	void initialize() {
+		start_simulation.setOnAction(actionEvent -> {
+			OperatingSystem.init();
+			changeScene("result-table");
+		});
+		generate_processes.setOnAction(actionEvent -> RequestGenerator.generateProcessData());
+		select_algorithms.setOnMouseClicked(mouseEvent -> changeScene("algorithm-selector"));
+		configure_process_generator.setOnMouseClicked(mouseEvent -> changeScene("generator-config"));
+		about.setOnMouseClicked(mouseEvent -> changeScene("about"));
 	}
 	
 	@Override
