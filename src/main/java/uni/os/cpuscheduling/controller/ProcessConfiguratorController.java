e76db874eee60faab9b11e6eeaf86c518eb290f2
Adjective10111 <mm.80xefatzadeh@gmail.com>
2022-01-05 18:07:52 +0330

    tweaked a bit

--- src/main/java/uni/os/cpuscheduling/controller/ProcessConfiguratorController.java
+++ src/main/java/uni/os/cpuscheduling/controller/ProcessConfiguratorController.java
@@ -2,15 +2,22 @@ package uni.os.cpuscheduling.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 
 public class ProcessConfiguratorController implements Controller {
 	@FXML
 	private ImageView back;
+	@FXML
+	private Button config;
 	
 	@FXML
 	void initialize() {
 		back.setOnMouseClicked(mouseEvent -> changeScene("main-menu"));
+		config.setOnAction(actionEvent -> {
+			// todo: config
+			changeScene("main-menu");
+		});
 	}
 	
 	@Override
--- src/main/java/uni/os/cpuscheduling/controller/ResultTableController.java
+++ src/main/java/uni/os/cpuscheduling/controller/ResultTableController.java
@@ -2,15 +2,21 @@ package uni.os.cpuscheduling.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 
 public class ResultTableController implements Controller {
 	@FXML
 	private ImageView back;
+	@FXML
+	private Button goto_chart;
 	
 	@FXML
 	void initialize() {
 		back.setOnMouseClicked(mouseEvent -> changeScene("main-menu"));
+		goto_chart.setOnAction(actionEvent -> changeScene("chart"));
+		
+		
 	}
 	
 	@Override
